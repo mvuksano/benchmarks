adce5f9156e5ecc2b73d58c2e3dd38dfb9f3d6fa
Marko Vuksanovic <markovuksanovic@gmail.com>
2023-04-14 14:53:52 -0700

    Change how AvxSum algorithm works.
    It is now aligned with AVX512 algorithm.
    
    ```
    ./mybenchmark --benchmark_filter="^(Avx)?Sum.*$" --benchmark_time_unit=ns
    2023-04-14T14:54:41-07:00
    Running ./mybenchmark
    Run on (8 X 2700 MHz CPU s)
    CPU Caches:
      L1 Data 32 KiB
      L1 Instruction 32 KiB
      L2 Unified 256 KiB (x4)
      L3 Unified 8192 KiB
    Load Average: 2.80, 3.19, 3.21
    -----------------------------------------------------
    Benchmark           Time             CPU   Iterations
    -----------------------------------------------------
    AvxSum256b       79.2 ns         79.0 ns      8447271
    AvxSum1K          232 ns          232 ns      2980207
    AvxSum4K          805 ns          804 ns       838946
    AvxSum8K         1599 ns         1596 ns       418038
    AvxSum1M       222110 ns       221758 ns         3207
    Sum256b           561 ns          560 ns      1252236
    Sum1K            2185 ns         2183 ns       315098
    Sum4K            8864 ns         8851 ns        76728
    Sum8K           17394 ns        17373 ns        39465
    Sum1M         2294319 ns      2289396 ns          293
    ```

--- perf.cc
+++ perf.cc
@@ -1,11 +1,11 @@
 #include <benchmark/benchmark.h>
 
-#define REGISTER_BENCHMARK(NAME, SIZE) static void NAME(benchmark::State &state) { \
+#define REGISTER_BENCHMARK(NAME, FN, SIZE) static void NAME(benchmark::State &state) { \
   auto s = SIZE; \
   float __attribute__((aligned(32))) vals[s]; \
   GenerateNumbers(vals, s); \
   for (auto _ : state) { \
-    auto res = sum(vals, s); \
+    auto res = FN(vals, s); \
     benchmark::DoNotOptimize(res); \
   } \
 } \
@@ -23,9 +23,7 @@ float avx512_sum(float *vals, size_t size) {
   float result = 0;
   float total = 0;
   float __attribute__((aligned(64))) temp[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-  asm(R"(
-          vmovdqa32   (%0), %%zmm0
-          vaddps %%zmm1, %%zmm2, %%zmm1)"
+  asm(R"(vmovdqa32   (%0), %%zmm0)"
       : /* no outputs */
       : "r"(&temp)
       : "zmm0", "ymm0", "xmm0");
@@ -49,23 +47,29 @@ float avx512_sum(float *vals, size_t size) {
 
 float avx_sum(float *vals, size_t size) {
   int __attribute__((aligned(32))) permute[] = {0, 4, 1, 5, 2, 6, 3, 7};
+  float __attribute__((aligned(32))) temp[] = {0, 0, 0, 0, 0, 0, 0, 0};
   float total = 0;
   float result = 0;
+  asm(R"(vmovaps (%0), %%ymm0)"
+	: /* outputs */
+	: "r"(&temp) 
+	: "xmm0", "ymm0");
   for (int i = 0; i < size / 8; i++) {
     asm(R"(
-            vmovaps    (%1), %%ymm0
-            vmovdqa32  (%2), %%ymm1
-            vhaddps %%ymm1, %%ymm1, %%ymm1
-            vhaddps %%ymm1, %%ymm1, %%ymm1
-            vpermps %%ymm1, %%ymm0, %%ymm1
-            vhaddps %%ymm1, %%ymm1, %%ymm1
-            vmovd %%xmm1, %0)"
-        : "=r"(result)
-        : "r"(&vals[i * 8]), "r"(&permute)
-        : "ymm0", "xmm0", "ymm1", "xmm1");
-    total += result;
+            vmovaps  (%0), %%ymm1
+            vhaddps %%ymm0, %%ymm1, %%ymm0)"
+        : /* no outputs */ 
+        : "r"(&vals[i * 8])
+        : "ymm0", "xmm0");
   }
+  asm(R"(vmovaps %%ymm1, %0)"
+	: "=m"(temp) 
+	: /* no inputs */
+	: /* clobbers */);
 
+  for (int i = 0; i < 8; i++) {
+	total += temp[i];
+  }
   return total;
 }
 
@@ -83,42 +87,11 @@ static void GenerateNumbers(float *nums, size_t size) {
   }
 }
 
-static void AvxSum256(benchmark::State &state) {
-  float __attribute__((aligned(32))) vals[256];
-  GenerateNumbers(vals, 256);
-  // Code inside this loop is measured repeatedly
-  for (auto _ : state) {
-    auto res = avx_sum(vals, 256);
-    benchmark::DoNotOptimize(res);
-  }
-}
-// Register the function as a benchmark
-BENCHMARK(AvxSum256);
-
-static void AvxSum1024(benchmark::State &state) {
-  float __attribute__((aligned(32))) vals[1024];
-  GenerateNumbers(vals, 1024);
-  // Code inside this loop is measured repeatedly
-  for (auto _ : state) {
-    auto res = avx_sum(vals, 1024);
-    benchmark::DoNotOptimize(res);
-  }
-}
-// Register the function as a benchmark
-BENCHMARK(AvxSum1024);
-
-static void AvxSum1024_x_1024(benchmark::State &state) {
-  auto s = 1024 * 1024;
-  float __attribute__((aligned(32))) vals[s];
-  GenerateNumbers(vals, s);
-  // Code inside this loop is measured repeatedly
-  for (auto _ : state) {
-    auto res = avx_sum(vals, s);
-    benchmark::DoNotOptimize(res);
-  }
-}
-// Register the function as a benchmark
-BENCHMARK(AvxSum1024_x_1024);
+REGISTER_BENCHMARK(AvxSum256b, avx_sum, 256);
+REGISTER_BENCHMARK(AvxSum1K, avx_sum, 1024);
+REGISTER_BENCHMARK(AvxSum4K, avx_sum, 4 * 1024);
+REGISTER_BENCHMARK(AvxSum8K, avx_sum, 8 * 1024);
+REGISTER_BENCHMARK(AvxSum1M, avx_sum, 1024 * 1024);
 
 static void Avx512Sum256(benchmark::State &state) {
   float __attribute__((aligned(64))) vals[256];
@@ -168,53 +141,9 @@ static void Avx512Sum4096(benchmark::State &state) {
 // Register the function as a benchmark
 BENCHMARK(Avx512Sum4096);
 
-static void Sum256(benchmark::State &state) {
-  float __attribute__((aligned(32))) vals[256];
-  GenerateNumbers(vals, 256);
-  // Code inside this loop is measured repeatedly
-  for (auto _ : state) {
-    auto res = sum(vals, 256);
-    benchmark::DoNotOptimize(res);
-  }
-}
-// Register the function as a benchmark
-BENCHMARK(Sum256);
-
-static void Sum1024(benchmark::State &state) {
-  float __attribute__((aligned(32))) vals[1024];
-  GenerateNumbers(vals, 1024);
-  // Code inside this loop is measured repeatedly
-  for (auto _ : state) {
-    auto res = sum(vals, 1024);
-    benchmark::DoNotOptimize(res);
-  }
-}
-// Register the function as a benchmark
-BENCHMARK(Sum1024);
-
-static void Sum4096(benchmark::State &state) {
-  float __attribute__((aligned(32))) vals[4096];
-  GenerateNumbers(vals, 4096);
-  // Code inside this loop is measured repeatedly
-  for (auto _ : state) {
-    auto res = sum(vals, 4096);
-    benchmark::DoNotOptimize(res);
-  }
-}
-// Register the function as a benchmark
-BENCHMARK(Sum4096);
-static void Sum1024_x_1024(benchmark::State &state) {
-  auto s = 1024 * 1024;
-  float __attribute__((aligned(32))) vals[s];
-  GenerateNumbers(vals, s);
-  // Code inside this loop is measured repeatedly
-  for (auto _ : state) {
-    auto res = sum(vals, s);
-    benchmark::DoNotOptimize(res);
-  }
-}
-// Register the function as a benchmark
-BENCHMARK(Sum1024_x_1024);
-
-REGISTER_BENCHMARK(Sum8192, 8192);
+REGISTER_BENCHMARK(Sum256b, sum, 256);
+REGISTER_BENCHMARK(Sum1K, sum, 1024);
+REGISTER_BENCHMARK(Sum4K, sum, 4096);
+REGISTER_BENCHMARK(Sum8K, sum, 8192);
+REGISTER_BENCHMARK(Sum1M, sum, 1024 * 1024);
 BENCHMARK_MAIN();
