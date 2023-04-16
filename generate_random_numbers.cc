cc4dc091937c9d8b9086b730d67baa14c40fa8ca
Marko Vuksanovic <markovuksanovic@gmail.com>
2023-04-16 13:37:29 -0700

    Random number generator program.

---
+++ generate_random_numbers.cc
@@ -0,0 +1,21 @@
+#include <immintrin.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+	int nums_fd = open("nums", O_CREAT | O_WRONLY | O_TRUNC);
+	if(nums_fd <0 ) {
+		printf("Could not open file \"nums\".\n");
+		exit(1);
+	}
+
+	for(int i = 0; i < 1024 * 1024; i++) {
+		uint64_t val;
+		auto res = _rdrand64_step(&val);
+		write(nums_fd, &val, sizeof(val));
+	}
+	close(nums_fd);
+	return 0;
+}
