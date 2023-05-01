6a716ac7bbf739ba5ff46bac1a91a816f77366ef
Marko Vuksanovic <markovuksanovic@gmail.com>
2023-05-01 08:27:54 -0700

    Generate random numbers using /dev/random instead of using intel intrinsics.

--- rng/generate_random_numbers.cc
+++ rng/generate_random_numbers.cc
@@ -1,21 +1,34 @@
 #include <fcntl.h>
-#include <immintrin.h>
 #include <inttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 int main(int argc, char **argv) {
-  int nums_fd = open("nums", O_CREAT | O_RDWR | O_TRUNC);
+  int nums_fd = open("nums", O_CREAT | O_RDWR | O_TRUNC, 0644);
   if (nums_fd < 0) {
     printf("Could not open file \"nums\".\n");
     exit(1);
   }
 
-  for (int i = 0; i < 1024 * 1024 * 64; i++) {
-    uint32_t val;
-    auto res = _rdrand32_step(&val);
-    write(nums_fd, &val, sizeof(val));
+  int rand_fd = open("/dev/random", O_RDONLY);
+  if (rand_fd < 0) {
+	  printf("Could not open /dev/random.");
+	  exit(1);
+  }
+
+  char buf[64];
+
+  for (int i = 0; i < 1024 * 1024; i++) {
+    if (read(rand_fd, buf, 64) != 64) {
+	    // If we didn't read 64 bytes then discard this block
+	    // and try again.
+	    i--;
+	    continue;
+    };
+    write(nums_fd, buf, 64);
   }
   close(nums_fd);
+  close(rand_fd);
   return 0;
 }
