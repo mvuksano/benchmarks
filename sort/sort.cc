ef5d9f826ab4b80ec920f2b719082891ce39419a
Marko Vuksanovic <markovuksanovic@gmail.com>
2023-05-05 17:21:10 +0000

    Load numbers for sum benchmark from 'generated' folder.

--- sort/sort.cc
+++ sort/sort.cc
@@ -13,7 +13,7 @@
 
 #define REGISTER_BENCHMARK(NAME, FN) static void NAME(benchmark::State &state) { \
   auto s = state.range(0); \
-  int nums_fd = open("nums", O_RDONLY); \
+  int nums_fd = open("generated/nums", O_RDONLY); \
   uint32_t *nums = (uint32_t *)mmap(NULL, 1024 * 1024 * 64 * sizeof(uint32_t), \
                           PROT_READ | PROT_WRITE, MAP_PRIVATE, nums_fd, 0); \
   for (auto _ : state) { \
