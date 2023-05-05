f11e7962c7ed0e3c46ce0149720a19bab302eb38
Marko Vuksanovic <markovuksanovic@gmail.com>
2023-05-05 16:47:45 +0000

    Disable building shared libs for glog and gflags.

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -xev 
+
 # Detect the operating system
 OS=$(uname)
 PROJECT_DIR=$(realpath $(dirname "$0"))
@@ -59,14 +61,14 @@ build_deps() {
 
   # Change to gflags directory
   cd "$(dirname "$0")/gflags"
-  cmake -B _build -S . -DBUILD_STATIC_LIBS=ON -DCMAKE_INSTALL_PREFIX=$LIB_INSTALL_DIR ${CPP_COMPILER:+-DCMAKE_CXX_COMPILER=$CPP_COMPILER} 
+  cmake -B _build -S . -DBUILD_STATIC_LIBS=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=$LIB_INSTALL_DIR ${CPP_COMPILER:+-DCMAKE_CXX_COMPILER=$CPP_COMPILER} 
   cmake --build _build
   cd _build
   make install
 
-  Change to glog directory
+  # Change to glog directory
   cd "$PROJECT_DIR/glog"
-  cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_INSTALL_PREFIX=$LIB_INSTALL_DIR ${CPP_COMPILER:+-DCMAKE_CXX_COMPILER=$CPP_COMPILER}
+  cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=$LIB_INSTALL_DIR ${CPP_COMPILER:+-DCMAKE_CXX_COMPILER=$CPP_COMPILER}
   cmake --build build
   cmake --build build --target install
 }
